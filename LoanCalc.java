e93713e67985264d1e1b322a28364998c6a039c2
MAROM DAVID <marom.david@post.runi.ac.il>
2024-11-27 16:14:57 +0200

    first

--- LoanCalc.java
+++ LoanCalc.java
@@ -28,8 +28,10 @@ public class LoanCalc {
 	// Computes the ending balance of a loan, given the loan amount, the periodical
 	// interest rate (as a percentage), the number of periods (n), and the periodical payment.
 	private static double endBalance(double loan, double rate, int n, double payment) {	
-		// Replace the following statement with your code
-		return 0;
+		for(int i = 0 ; i < n ; i++){
+		loan = (loan - payment)*(1+(rate/100));
+		}
+		return loan;
 	}
 	
 	// Uses sequential search to compute an approximation of the periodical payment
@@ -38,17 +40,41 @@ public class LoanCalc {
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		// Replace the following statement with your code
-		return 0;
+		double payment = loan/n;
+		double balance = endBalance(loan, rate, n, payment);
+
+		while (balance > epsilon) {
+			payment += epsilon;
+			balance = endBalance(loan, rate, n, payment);
+			iterationCounter ++;
+		}
+		return payment;
     }
-    
     // Uses bisection search to compute an approximation of the periodical payment 
 	// that will bring the ending balance of a loan close to 0.
 	// Given: the sum of the loan, the periodical interest rate (as a percentage),
 	// the number of periods (n), and epsilon, the approximation's accuracy
 	// Side effect: modifies the class variable iterationCounter.
     public static double bisectionSolver(double loan, double rate, int n, double epsilon) {  
-        // Replace the following statement with your code
-		return 0;
+        double L = loan/n;
+		double H = loan*(1+rate);
+		double g = 0;
+
+		iterationCounter = 0;
+
+		while(H - L > epsilon)
+		{
+			iterationCounter ++;
+			g = (L + H)/2;
+			double balance = endBalance(loan, rate, n, g);
+			if(balance > 0)
+			{
+				L = g;
+			}
+			else{
+				H = g;				
+			}
+		}
+		return g;
     }
 }
