5064fca6e939a5a7ae31f46ab60610f8fe429891
MAROM DAVID <marom.david@post.runi.ac.il>
2024-11-28 10:44:31 +0200

    first

--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,57 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String afterStr1 = preProcess(str1);
+		String afterStr2 = preProcess(str2);
+
+		while(afterStr1.length() > 0){
+			int count = 0;
+			if(afterStr2.indexOf(afterStr1.charAt(count)) == -1){
+				return false;
+			}
+				count ++;
+				afterStr1 = afterStr1.substring(count);
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		String allowString = "abcdefghijklmnopqrstuvwxyz ";
+		String newStr = str.toLowerCase();
+		String newestString  = "";
+		for(int i = 0; i < newStr.length(); i++){
+			char currentChar = newStr.charAt(i);
+			if (allowString.indexOf(currentChar) > -1) {
+				newestString += currentChar;
+			}
+		}
+		return newestString;
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		
+		str = preProcess(str); // Assume this method is defined elsewhere
+		char[] charArray = str.toCharArray();
+	
+		// Shuffle the characters using Math.random()
+		for (int i = charArray.length - 1; i > 0; i--) {
+			int j = (int) (Math.random() * (i + 1)); // Random index from 0 to i
+			// Swap characters
+			char temp = charArray[i];
+			charArray[i] = charArray[j];
+			charArray[j] = temp;
+		}
+	
+		// Return the shuffled characters as a new string
+		//return new String(charArray);
+	
+
+		return new String(charArray);
 	}
 }
+
